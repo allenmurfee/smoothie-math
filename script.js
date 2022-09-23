40b726010cf81ac07449f169da41d88dd1a749ec
allenmurfee <allenmurfee123@gmail.com>
2022-09-22 23:19:34 -0400

    API working

--- script.js
+++ script.js
@@ -21,17 +21,15 @@ function testAPI() {
           //   console.log(data.common[0].food_name);
           foodName = data.common[0].food_name;
           console.log(foodName);
-          $.ajax({
-            url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
+          fetch("https://trackapi.nutritionix.com/v2/natural/nutrients", {
             method: "POST",
             headers: {
-              "Content-Type": "application-json;charset=UTF-8",
+              "Content-Type": "application/json",
               "x-app-id": "def8c961",
               "x-app-key": "751bb1c983bf066a6d7484cdf84838e4",
-              "x-remote-user-id": "0",
             },
             body: JSON.stringify({
-              query: "apple",
+              query: item,
               num_servings: 0,
               line_delimited: false,
               use_raw_foods: false,
@@ -46,39 +44,11 @@ function testAPI() {
               last_modified: false,
             }),
           })
-            //   fetch("https://trackapi.nutritionix.com/v2/natural/nutrients", {
-            //     method: "POST",
-            //     headers: {
-            //       "content-type": "application-json",
-            //       "x-app-id": "def8c961",
-            //       "x-app-key": "751bb1c983bf066a6d7484cdf84838e4",
-            //       "x-remote-user-id": "0",
-            //     },
-            //     body: JSON.stringify({
-            //       query: "apple",
-            //       num_servings: 0,
-            //       line_delimited: false,
-            //       use_raw_foods: false,
-            //       include_subrecipe: false,
-            //       lat: 0,
-            //       lng: 0,
-            //       meal_type: 0,
-            //       use_branded_foods: false,
-            //       locale: "en_US",
-            //       taxonomy: false,
-            //       ingredient_statement: false,
-            //       last_modified: false,
-            //     }),
-            //   })
             .then(function (response) {
-              console.log(response);
-              if (response.ok) {
-                response.json().then(function (data) {
-                  console.log(data);
-                });
-              } else {
-                alert("Error: " + response.statusText);
-              }
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(data);
             })
             .catch(function (error) {
               console.log(error);
