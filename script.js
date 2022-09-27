41bd25f4a98103fc0e55762ab1be7b5b68177eb0
allenmurfee <allenmurfee123@gmail.com>
2022-09-26 20:43:29 -0400

    adding nutrition when click mix it button

--- script.js
+++ script.js
@@ -96,71 +96,48 @@ function getNutrients(food) {
     });
 }
 
-//   var item = "apple"; //user input
-//   var apiUrl =
-//     "https://trackapi.nutritionix.com/v2/search/instant?query=" + item;
-//   var foodName = "";
-
-//   //   var nutriURL =
-//   //     "https://trackapi.nutritionix.com/v2/natural/nutrients?query=" + item;
-
-//   fetch(apiUrl, {
-//     headers: {
-//       "x-app-id": "def8c961",
-//       "x-app-key": "751bb1c983bf066a6d7484cdf84838e4",
-//     },
-//   })
-//     .then(function (response) {
-//       if (response.ok) {
-//         response.json().then(function (data) {
-//           //   console.log(data.common[0].food_name);
-//           foodName = data.common[0].food_name;
-//           console.log(foodName);
-//           fetch("https://trackapi.nutritionix.com/v2/natural/nutrients", {
-//             method: "POST",
-//             headers: {
-//               "Content-Type": "application/json",
-//               "x-app-id": "def8c961",
-//               "x-app-key": "751bb1c983bf066a6d7484cdf84838e4",
-//             },
-//             body: JSON.stringify({
-//               query: item,
-//               num_servings: 0,
-//               line_delimited: false,
-//               use_raw_foods: false,
-//               include_subrecipe: false,
-//               lat: 0,
-//               lng: 0,
-//               meal_type: 0,
-//               use_branded_foods: false,
-//               locale: "en_US",
-//               taxonomy: false,
-//               ingredient_statement: false,
-//               last_modified: false,
-//             }),
-//           })
-//             .then(function (response) {
-//               return response.json();
-//             })
-//             .then(function (data) {
-//               console.log(data);
-//             })
-//             .catch(function (error) {
-//               console.log(error);
-//               //   alert("Unable to gather data.");
-//             });
-//         });
-//       } else {
-//         alert("Error: " + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       alert("Unable to gather data.");
-//     });
+// function calculate() {
+//   for (var i = 0; i < foodNutrition.calories.length; i++) {
+//     var calSum = 0;
+//     calSum += foodNutrition.calories[i];
+//     console.log(calSum);
+//   }
 // }
 
 searchBtn.on("click", function () {
   searchFood(inputBox.val());
 });
 
-mixBtn.on("click", function () {});
+mixBtn.on("click", function() {
+  var calSum = 0;
+  var fiberSum = 0;
+  var proteinSum = 0;
+  var carbSum = 0;
+  var sugarSum = 0;
+  for (var i = 0; i < foodNutrition.calories.length; i++) {
+    calSum += foodNutrition.calories[i];
+  };
+  for (var i = 0; i < foodNutrition.fiber.length; i++) {
+    fiberSum += foodNutrition.fiber[i];
+  };
+  for (var i = 0; i < foodNutrition.protein.length; i++) {
+    proteinSum += foodNutrition.protein[i];
+  };
+  for (var i = 0; i < foodNutrition.carbs.length; i++) {
+    carbSum += foodNutrition.carbs[i];
+  };
+  for (var i = 0; i < foodNutrition.sugar.length; i++) {
+    sugarSum += foodNutrition.sugar[i];
+
+    
+  // for (var i = 0; i < foodNutrition.calories.length; i++) {
+  //   var calSum = 0;
+  //   calSum += foodNutrition.calories[i];
+  //   console.log(calSum);
+}
+console.log(calSum);
+console.log(fiberSum);
+console.log(proteinSum);
+console.log(carbSum);
+console.log(sugarSum);
+});
